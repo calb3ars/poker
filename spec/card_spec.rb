4954653abc374924cb53fd9c3ec723088eb28823
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-02-01 21:29:07 -0800

    sync rspec files with a/A solutions. Use to prep for assessment 2

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,24 +1,47 @@
+require 'rspec'
 require 'card'
 
 describe Card do
-subject(:card) { Card.new(3,:club) }
-  describe "#initialize" do
-    it "has a number" do
-      expect(card.number).to eq(3)
+  describe '#initialize' do
+    subject(:card) { Card.new(:spades, :ten) }
+
+    it 'sets up a card correctly' do
+      expect(card.suit).to eq(:spades)
+      expect(card.value).to eq(:ten)
     end
 
-    it "has a suit" do
-      expect(card.suit).to eq(:club)
+    it 'raises an error with an invalid suit' do
+      expect do
+        Card.new(:test, :ten)
+      end.to raise_error
     end
 
-    it "raises an error if number is greater than 13" do
-      expect{Card.new(20, :heart)}.to raise_error("Card not valid, too high")
+    it 'raises an error with an invalid value' do
+      expect do
+        Card.new(:spades, :test)
+      end.to raise_error
     end
-
-    it "raises an error if suit is invalid" do
-      expect{Card.new(4, :racecar)}.to raise_error("Card not valid, invalid suit")
-    end
-
   end
 
+  describe '#<=>' do
+    it 'should return 0 when cards are the same' do
+      expect(Card.new(:spades, :ten) <=> Card.new(:spades, :ten)).to eq(0)
+    end
+
+    it 'should return 1 when card has higher value' do
+      expect(Card.new(:spades, :ace) <=> Card.new(:spades, :ten)).to eq(1)
+    end
+
+    it 'should return 1 when card has same value but higher suit' do
+      expect(Card.new(:spades, :ace) <=> Card.new(:hearts, :ace)).to eq(1)
+    end
+
+    it 'should return -1 when card has lower value' do
+      expect(Card.new(:spades, :ten) <=> Card.new(:spades, :ace)).to eq(-1)
+    end
+
+    it 'should return -1 when card has same value but lower suit' do
+      expect(Card.new(:hearts, :ace) <=> Card.new(:spades, :ace)).to eq(-1)
+    end
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,43 +1,124 @@
-require "deck"
+require 'rspec'
+require 'deck'
 
 describe Deck do
-  subject(:deck) { Deck.new }
+  describe "::all_cards" do
+    subject(:all_cards) { Deck.all_cards }
+
+    it "should generate 52 cards" do
+      expect(all_cards.count).to eq(52)
+    end
+
+    it "returns all cards without duplicates" do
+      expect(
+        all_cards.map { |card| [card.suit, card.value] }.uniq.count
+      ).to eq(all_cards.count)
+    end
+  end
+
+  let(:cards) do
+    [ double("card", :suit => :spades, :value => :king),
+      double("card", :suit => :spades, :value => :queen),
+      double("card", :suit => :spades, :value => :jack) ]
+  end
 
   describe "#initialize" do
-    it "should initialize to have a standard 52-card deck" do
-      expect(deck.cards.length).to eq(52)
+    it "by default fills itself with 52 cards" do
+      deck = Deck.new
+      expect(deck.count).to eq(52)
     end
 
-    it "should only include unique cards" do
-      expect(deck.cards.uniq).to eq(deck.cards)
-    end
-
-  end
-
-  describe "#deal" do
-    let(:dealt_card) { Deck.new.cards.last }
-    it "should return the top card in the deck" do
-      expect(deck.deal.values).to eq(dealt_card.values)
-    end
-
-    it "should remove returned cards from the deck" do
-      deck.deal
-      expect(deck.cards).to_not include(dealt_card)
+    it "can be initialized with an array of cards" do
+      deck = Deck.new(cards)
+      expect(deck.count).to eq(3)
     end
   end
 
-  describe "#shuffle" do
-    let(:preshuffled_deck) { Deck.new }
-    it "should return a shuffled deck of cards" do
-      deck.shuffle
-      expect(deck.values).to_not eq(preshuffled_deck.values)
+  let(:deck) do
+    Deck.new(cards.dup)
+  end
+
+  it "should not expose its cards" do
+    expect(deck).not_to respond_to(:cards)
+  end
+
+  describe "#take" do
+    # **use the front of the cards array as the top**
+    it "takes cards off the top of the deck" do
+      expect(deck.take(1)).to eq(cards[0..0])
+      expect(deck.take(2)).to eq(cards[1..2])
+    end
+
+    it "removes cards from deck on take" do
+      deck.take(2)
+      expect(deck.count).to eq(1)
+    end
+
+    it "doesn't allow you to take more cards than are in the deck" do
+      expect do
+        deck.take(4)
+      end.to raise_error("not enough cards")
     end
   end
 
-  describe "#reset" do
-    it "should reset deck to include standard 52 cards" do
-      deck.reset
-      expect(deck.cards.length).to eq(52)
+  describe "#return" do
+    let(:more_cards) do
+      [ double("card", :suit => :hearts, :value => :four),
+        double("card", :suit => :hearts, :value => :five),
+        double("card", :suit => :hearts, :value => :six) ]
+    end
+
+    it "should return cards to the deck" do
+      deck.return(more_cards)
+      expect(deck.count).to eq(6)
+    end
+
+    it "should not destroy the passed array" do
+      more_cards_dup = more_cards.dup
+      deck.return(more_cards_dup)
+      expect(more_cards_dup).to eq(more_cards)
+    end
+
+    it "should add new cards to the bottom of the deck" do
+      deck.return(more_cards)
+      deck.take(3) # toss 3 cards away
+
+      expect(deck.take(1)).to eq(more_cards[0..0])
+      expect(deck.take(1)).to eq(more_cards[1..1])
+      expect(deck.take(1)).to eq(more_cards[2..2])
+    end
+  end
+
+  describe '#shuffle' do
+    it 'should shuffle the cards' do
+      cards = deck.take(3)
+      deck.return(cards)
+
+      not_shuffled = (1..5).all? do
+        deck.shuffle
+        shuffled_cards = deck.take(3)
+        deck.return(shuffled_cards)
+
+        (0..2).all? { |i| shuffled_cards[i] == cards[i] }
+      end
+
+      expect(not_shuffled).to be(false)
+    end
+  end
+
+  describe '#deal_hand' do
+    let(:deck) { Deck.new }
+
+    it 'should return a new hand' do
+      hand = deck.deal_hand
+      expect(hand).to be_a(Hand)
+      expect(hand.cards.count).to eq(5)
+    end
+
+    it 'should take cards from the deck' do
+      expect do
+        deck.deal_hand
+      end.to change{ deck.count }.by(-5)
     end
   end
 end
